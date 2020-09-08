88b963b64f06028233bd997d6bd2bb36bd89b49b
Danil Rukin <danil2018d@yandex.ru>
2020-09-09 00:16:43 +0700

    Начал делать лабу 1 по сортировке пирамидой

---
+++ BinaryHeap/BinaryHeap.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PyramidSort.BinaryHeap
+{
+	public class BinaryHeap<T> where T : IComparable
+	{
+		private T[] nodes;
+		private const int MAX_SIZE = 10000;
+		private int currentHeapSize;
+
+		public BinaryHeap()
+		{
+			currentHeapSize = 0;
+			nodes = new T[MAX_SIZE];
+		}
+
+		public void Add(T nodeToAdd)
+		{
+			if (nodeToAdd == null || IsFull())
+			{
+				return;
+			}
+			nodes[currentHeapSize] = nodeToAdd;
+			RestoreProperty(currentHeapSize);
+			currentHeapSize++;
+		}
+
+		private bool IsFull()
+		{
+			return currentHeapSize >= MAX_SIZE;
+		}
+
+		private void RestoreProperty(int lastAddedNodeIndex)
+		{
+			int parent = (lastAddedNodeIndex - 1) / 2; // lastAddedNodeIndex == 8 => parent == 3 or if lastAddedNodeIndex == 7 => parent == 3
+			int currentIndex = lastAddedNodeIndex;
+			while (parent >= 0 && currentIndex > 0)
+			{
+				if (nodes[currentIndex] > nodes[parent]) // substitute
+				{
+					Swap(currentIndex, parent);
+				}
+				currentIndex = parent;
+				parent = (parent - 1) / 2;
+			}
+		}
+
+		private void Swap(int firstIndex, int secondIndex)
+		{
+			T tmp = nodes[firstIndex];
+			nodes[firstIndex] = nodes[secondIndex];
+			nodes[secondIndex] = tmp;
+		}
+
+		private void Heapyfi(int startIndex)
+		{
+			int leftChildIndex = startIndex * 2 + 1;
+			int rightChildIndex = startIndex * 2 + 2;
+			int largestNodeIndex = startIndex;
+
+			while (true)
+			{
+				if (leftChildIndex < currentHeapSize && nodes[leftChildIndex] > nodes[largestNodeIndex])  // substitute
+				{
+					largestNodeIndex = leftChildIndex;
+				}
+				if (rightChildIndex < currentHeapSize && nodes[rightChildIndex] > nodes[largestNodeIndex]) //substitute
+				{
+					largestNodeIndex = rightChildIndex;
+				}
+				if (largestNodeIndex == startIndex)
+				{
+					break;
+				}
+				Swap(startIndex, largestNodeIndex);
+				startIndex = largestNodeIndex;
+			}
+		}
+	}
+}
---
+++ BinaryHeap/Node.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestsGeneration;
+
+namespace PyramidSort.BinaryHeap
+{
+	public class Node : IGeneratable, IComparable
+	{
+		public string FirstField { get; set; }
+		public string SecondField { get; set; }
+
+		public int ThirdField { get; set; }
+		public int FourthField { get; set; }
+		public int FifthField { get; set; }
+
+		public int CompareTo(object obj)
+		{
+			Node toCompare = obj as Node;
+			if (toCompare == null)
+			{
+				throw new Exception("Invalid type in 'CompareTo()' method inside 'Node' class");
+			}
+			else
+			{
+
+			}
+		}
+
+		public void Randomize()
+		{
+			Random random = new Random();
+			ThirdField = random.Next();
+			FourthField = random.Next();
+			FifthField = random.Next();
+			FirstField = StringExtension.CreateRandomString(FirstField, random.Next(1, GlobalConstants.STRING_MAX_LENGHT));
+			SecondField = StringExtension.CreateRandomString(FirstField, random.Next(1, GlobalConstants.STRING_MAX_LENGHT));
+		}
+	}
+}
